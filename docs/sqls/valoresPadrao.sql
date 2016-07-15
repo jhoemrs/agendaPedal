3e9cc5b27a6d2bddcf088e47ecbe1351d1df81f4
Jhonatan Mark <jhoemrs@gmail.com>
2016-07-14 21:39:12 -0300

    Valores Padrao Fix.

--- docs/sqls/valoresPadrao.sql
+++ docs/sqls/valoresPadrao.sql
@@ -1,12 +1,12 @@
-INSERT INTO `agendapedal`.`tipo_pedal` (`codigo`, `nome`) VALUES ('asfalto', 'Asfalto');
-INSERT INTO `agendapedal`.`tipo_pedal` (`codigo`, `nome`) VALUES ('mtb', 'Mountain Bike');
-INSERT INTO `agendapedal`.`tipo_pedal` (`codigo`, `nome`) VALUES ('misto', 'Misto');
+INSERT INTO `agendapedal`.`tipo_pedal` (`id`,`codigo`, `nome`) VALUES ('1','asfalto', 'Asfalto');
+INSERT INTO `agendapedal`.`tipo_pedal` (`id`,`codigo`, `nome`) VALUES ('2','mtb', 'Mountain Bike');
+INSERT INTO `agendapedal`.`tipo_pedal` (`id`,`codigo`, `nome`) VALUES ('3','misto', 'Misto');
 
-INSERT INTO `agendapedal`.`ritmo_pedal` (`codigo`, `nome`) VALUES ('leve', 'Leve');
-INSERT INTO `agendapedal`.`ritmo_pedal` (`codigo`, `nome`) VALUES ('medio', 'Médio');
-INSERT INTO `agendapedal`.`ritmo_pedal` (`codigo`, `nome`) VALUES ('intenso', 'Intenso');
+INSERT INTO `agendapedal`.`ritmo_pedal` (`id`,`codigo`, `nome`) VALUES ('1','leve', 'Leve');
+INSERT INTO `agendapedal`.`ritmo_pedal` (`id`,`codigo`, `nome`) VALUES ('2','medio', 'Médio');
+INSERT INTO `agendapedal`.`ritmo_pedal` (`id`,`codigo`, `nome`) VALUES ('3','intenso', 'Intenso');
 
-INSERT INTO `agendapedal`.`distancia_pedal` (`codigo`, `nome`) VALUES ('curta', 'Curta - Até 30 Km');
-INSERT INTO `agendapedal`.`distancia_pedal` (`codigo`, `nome`) VALUES ('media', 'Média - 30 a 80 Km');
-INSERT INTO `agendapedal`.`distancia_pedal` (`codigo`, `nome`) VALUES ('mediana', 'Mediana - 80 a 150 Km');
-INSERT INTO `agendapedal`.`distancia_pedal` (`codigo`, `nome`) VALUES ('longa', 'Longa - Acima de 150 Km');
+INSERT INTO `agendapedal`.`distancia_pedal` (`id`,`codigo`, `nome`) VALUES ('1','curta', 'Curta - Até 30 Km');
+INSERT INTO `agendapedal`.`distancia_pedal` (`id`,`codigo`, `nome`) VALUES ('2','media', 'Média - 30 a 80 Km');
+INSERT INTO `agendapedal`.`distancia_pedal` (`id`,`codigo`, `nome`) VALUES ('3','mediana', 'Mediana - 80 a 150 Km');
+INSERT INTO `agendapedal`.`distancia_pedal` (`id`,`codigo`, `nome`) VALUES ('4','longa', 'Longa - Acima de 150 Km');
